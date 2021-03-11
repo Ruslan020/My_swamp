911dca865d4405b699ff740e17156f9cec986c69
Ruslan020 <79364379+Ruslan020@users.noreply.github.com>
2021-03-11 12:02:04 +0300

    Add files via upload

---
+++ Dll + EXE Reflection/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        private static Dictionary<string,int> FindWords(string line)
+        {
+            Dictionary<string, int> words = new Dictionary<string, int>();
+            string word = "";
+            foreach (char letter in line)
+            {
+                int asciiLetter = (int)letter;
+                if ((asciiLetter > 64 && asciiLetter < 91) || (asciiLetter > 96 && asciiLetter < 123)
+                    || (asciiLetter > 1039 && asciiLetter < 1103) || asciiLetter == 1025 || asciiLetter == 1105)
+                {
+                    word += letter;
+                }
+                else
+                {
+                    word = word.ToLower();
+                    if (words.Keys.Contains(word)) words[word] += 1;
+                    else words.Add(word, 1);
+                    word = "";
+                }
+            }
+            return words;
+        }
+    }
+}
---
+++ Dll + EXE Reflection/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace WorldAndWarEXE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Dictionary<string, int> Words = new Dictionary<string, int>();
+            //чтение файла и поиск слов
+            string readPath = @"D:\Digital Design\DigDesignWithDll\WorldAndWarEXE\ВойнаИМир.txt";
+            try
+            {
+                
+                using (System.IO.StreamReader sr = new System.IO.StreamReader(readPath, System.Text.Encoding.Default))
+                {
+                    string line = sr.ReadToEnd();
+                    
+                    Class1 lib = new Class1();
+                    Console.WriteLine("Чтение выолнено1");
+                    var wordsInLine = lib.GetType().GetMethod("FindWords", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+                    Console.WriteLine("Чтение выолнено2");
+                    Words =  (Dictionary<string, int>)wordsInLine.Invoke(lib, new object[] {line });
+                    Console.WriteLine("Чтение выолнено3");
+                    /*wordsInLine(line);
+                        
+                    if (Words.ContainsKey(word))
+                    {
+                        Words[word] += 1;
+                    }
+                    else Words[word] = 1;
+                    word = "";*/
+                    
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            //Запись ответа
+            string writePath = @"D:\Digital Design\DigDesignWithDll\WorldAndWarEXE\ВойнаИМир-повторяющиеся слова.txt";
+
+            try
+            {
+
+                using (System.IO.StreamWriter sw = new System.IO.StreamWriter(writePath, false, System.Text.Encoding.Default))
+                {
+                    foreach (KeyValuePair<string, int> keyValue1 in Words.OrderByDescending(keyValue1 => keyValue1.Value))
+                    {
+                        sw.WriteLine(keyValue1.Key + " - " + keyValue1.Value);
+                    }
+                }
+
+                Console.WriteLine("Слова и количество их использования записаны\nФайл: " +  writePath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
